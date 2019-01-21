22186e113254616152a08458e4bbd7f468e57ba6
cheyuriy <yrii167167@gmail.com>
2019-01-21 20:25:43 +0300

    softmax finished

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -22,6 +22,9 @@ def softmax_loss_naive(W, X, y, reg):
   # Initialize the loss and gradient to zero.
   loss = 0.0
   dW = np.zeros_like(W)
+    
+  n = X.shape[0]
+  c = W.shape[1]
 
   #############################################################################
   # TODO: Compute the softmax loss and its gradient using explicit loops.     #
@@ -29,7 +32,20 @@ def softmax_loss_naive(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  for i in range(n):
+      zs = W.T.dot(X[i,:])
+      zs -= np.max(zs)
+      sample_softmax = np.exp(zs) / np.sum(np.exp(zs))
+      sample_loss = - np.log(sample_softmax[y[i]])
+      loss += sample_loss
+      for j in range(c):
+          dW[:, j] += (sample_softmax[j] - int(j == y[i])) * X[i,:]
+      
+  loss /= n
+  dW /= n
+    
+  loss += 0.5 * reg * np.sum(W * W)
+  dW += reg*W
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
@@ -46,14 +62,24 @@ def softmax_loss_vectorized(W, X, y, reg):
   # Initialize the loss and gradient to zero.
   loss = 0.0
   dW = np.zeros_like(W)
-
+  n = X.shape[0]
+  c = W.shape[1]
+    
   #############################################################################
   # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
   # Store the loss in loss and the gradient in dW. If you are not careful     #
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  z = np.matmul(X, W)
+  z -= np.max(z, axis=1).reshape((n,1))
+  sm = np.exp(z)/np.sum(np.exp(z), axis = 1).reshape((n,1))
+  loss = np.sum(-np.log(sm[np.arange(n),y]))/n
+  sm[np.arange(n),y] -= 1
+  dW = np.matmul(X.T, sm)/n
+    
+  loss += 0.5 * reg * np.sum(W * W)
+  dW += reg*W
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
